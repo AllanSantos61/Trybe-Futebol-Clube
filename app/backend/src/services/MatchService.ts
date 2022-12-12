e02b23e852277a541644915010421392bf33a51f
Allan <61allansantos@gmail.com>
2022-12-12 18:16:30 -0300

    Match

--- app/backend/src/services/MatchService.ts
+++ app/backend/src/services/MatchService.ts
@@ -1,40 +1,77 @@
-import { compare } from 'bcryptjs';
+import { ICreateMatch, IMatch, IUpMatch } from '../interfaces/IMatch';
 import HttpException from '../utils/HttpExecpetion';
-import Token from '../utils/Token';
-import ILogin from '../interfaces/ILogin';
-import User from '../database/models/User';
-import { loginSchema } from './validations/schemas';
+import Match from '../database/models/Match';
+import Team from '../database/models/Team';
+import { matchSchema } from './validations/schemas';
 
-export default class UserService {
-  private _userModel = User;
-  private _token = Token;
+const INCLUDE_OPTIONS = [
+  { model: Team, as: 'teamHome', attributes: ['teamName'] },
+  { model: Team, as: 'teamAway', attributes: ['teamName'] },
+];
 
-  private static validateLoginSchema(credentials: ILogin): void {
-    const { error } = loginSchema.validate(credentials);
-    if (error) {
-      const statusCode = error.message.includes('email') ? 401 : 400;
-      throw new HttpException(statusCode, error.message);
-    }
-  }
+export default class MatchService {
+  private _matchModel = Match;
+  private _teamModel = Team;
 
-  async loginService(credentials: ILogin): Promise<string> {
-    UserService.validateLoginSchema(credentials);
-
-    const user = await this._userModel.findOne({
-      where: { email: credentials.email },
+  async getAll(): Promise<IMatch[]> {
+    const matches = await this._matchModel.findAll({
+      include: INCLUDE_OPTIONS,
     });
-
-    if (!user || !(await compare(credentials.password, user.password))) {
-      throw new HttpException(401, 'Incorrect email or password');
-    }
-
-    const token = await this._token.generate(user);
-    return token;
+    return matches;
   }
 
-  async getRoleService(id: number): Promise<string> {
-    const user = await this._userModel.findOne({ where: { id } });
-    if (!user) throw new HttpException(404, 'User not found');
-    return user.role;
+  async getByProgress(status: string): Promise<IMatch[]> {
+    const inProgress = status === 'true';
+    const matches = await this._matchModel.findAll({
+      include: INCLUDE_OPTIONS,
+      where: { inProgress },
+    });
+    return matches;
+  }
+
+  private static validateMatchSchema(match: ICreateMatch): void {
+    const { error } = matchSchema.validate(match);
+    if (error) throw new HttpException(400, 'All fields must be filled');
+  }
+
+  private async validateMatchTeams({
+    homeTeam,
+    awayTeam,
+  }: ICreateMatch) {
+    if (homeTeam === awayTeam) {
+      throw new HttpException(
+        422,
+        'It is not possible to create a match with two equal teams',
+      );
+    }
+
+    const validHomeTeam = await this._teamModel.findByPk(homeTeam);
+    const validAwayTeam = await this._teamModel.findByPk(awayTeam);
+    if (!validHomeTeam || !validAwayTeam) {
+      throw new HttpException(404, 'There is no team with such id!');
+    }
+  }
+
+  async create(match: ICreateMatch): Promise<IMatch> {
+    MatchService.validateMatchSchema(match);
+    await this.validateMatchTeams(match);
+
+    const newMatch = await this._matchModel.create({
+      ...match, inProgress: true,
+    });
+    return newMatch;
+  }
+
+  async finish(id: number): Promise<void> {
+    const [result] = await this._matchModel.update(
+      { inProgress: false },
+      { where: { id } },
+    );
+    if (result !== 1) throw new HttpException(404, 'Update unsuccessful');
+  }
+
+  async update(values: IUpMatch, id: number): Promise<void> {
+    const [result] = await this._matchModel.update(values, { where: { id } });
+    if (result !== 1) throw new HttpException(404, 'Update unsuccessful');
   }
 }
