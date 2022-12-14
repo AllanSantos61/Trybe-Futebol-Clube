a6d9894a1b089c1299b75c0c72013605ff58502f
Allan <61allansantos@gmail.com>
2022-12-14 13:15:35 -0300

    PRÉ MONITORIA

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,43 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import { Response } from 'superagent';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import App from '../app';
+
+import { StatusCodes } from 'http-status-codes';
+
+chai.use(chaiHttp);
+
+const { app } = new App();
+
+const { expect } = chai;
+
+describe('/leaderboard', () => {
+  let chaiHttpResponse: Response;
+
+  describe('GET', () => {
+    test('RETURN OK', async () => {
+      chaiHttpResponse = await chai.request(app).get('/leaderboard');
+
+      expect(chaiHttpResponse.status).to.be.equal(StatusCodes.OK);
+    }); 
+  });
+
+  describe('/leaderboard/home', () => {
+    test('RETURN HOME', async () => {
+      chaiHttpResponse = await chai.request(app).get('/leaderboard/home');
+
+      expect(chaiHttpResponse.status).to.be.equal(StatusCodes.OK);
+    }); 
+  });
+
+  describe('/leaderboard/away', () => {
+    test('RETURN AWAY', async () => {
+      chaiHttpResponse = await chai.request(app).get('/leaderboard/away');
+
+      expect(chaiHttpResponse.status).to.be.equal(StatusCodes.OK);
+    }); 
+  });
+});
